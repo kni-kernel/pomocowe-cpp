d4cba6396aa0b680da1f43af7db2096257d64614
Griger5 <griger04@gmail.com>
2026-04-14 19:14:08 +0200

    add: next exercises

---
+++ blank/10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+// Program ma wytworzyć poniższy output.
+
+/* 
+    MIEJSCE NA KOD 
+*/
+
+int main(){
+
+    auto vadd2 = liftToVector();
+    std::vector<int> x = {0,9,4};
+    auto v = vadd2(x);
+
+    std::for_each(v.begin(), v.end(), [](const int n) {std::cout<< n << ' ';});
+
+    return 0;
+}
+
+// 2 11 6
---
+++ blank/11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// Program ma wytworzyć poniższy output.
+
+/* 
+    MIEJSCE NA KOD 
+*/
+
+int main()
+{
+    typedef ::OOP::Array<int> type;
+    type a(rand() % 100 + 10); // nie trzeba seedować żeby program się wykonał
+
+    a.insert('#' - 1).insert('P') % type::value_type('O') % ('-') % ('E') % ('G') % ('Z') % ('1');
+
+     ++a[0];
+    for (unsigned i = 0; i != ~a; i++){
+        std::cout << static_cast<char>(a[i]) << (i + 1 != ~a ? '_' : '\n');
+    }
+}
+//#_P_O_-_E_G_Z_1
